abbc3f2883a7d0cf568d8dcb033c00ae8f27b89d
nappannda <qwe159753qwee@gmail.com>
2018-01-28 05:38:54 +0900

    Add Project

---
+++ SampleFillItemTabBarItem/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SampleFillItemTabBarItem
+//
+//  Created by 上原和輝 on 2018/01/28.
+//  Copyright © 2018年 nappannda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SampleFillItemTabBarItemTests/SampleFillItemTabBarItemTests.swift
@@ -0,0 +1,36 @@
+//
+//  SampleFillItemTabBarItemTests.swift
+//  SampleFillItemTabBarItemTests
+//
+//  Created by 上原和輝 on 2018/01/28.
+//  Copyright © 2018年 nappannda. All rights reserved.
+//
+
+import XCTest
+@testable import SampleFillItemTabBarItem
+
+class SampleFillItemTabBarItemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
